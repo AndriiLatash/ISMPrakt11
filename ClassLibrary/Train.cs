a61fec3100b23181e70ba78917284ae0d48185c2
Andrii <latasandrej@gmail.com>
2022-02-17 00:02:18 +0200

    Created class Train

---
+++ ClassLibrary/Train.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Train : Transport
+    {
+
+        public double NumberOfWagons { get; set; }
+        public string Manufacturer { get; set; }
+        public Train(double numberOfWagons, double kilometers, string manufacturer)
+        {
+            Kilometers = kilometers;
+            NumberOfWagons = numberOfWagons;
+            Manufacturer = manufacturer;
+        }
+        public Train(string visitedPlaces)
+        {
+            VisitedPlaces = visitedPlaces;
+        }
+        public override double GetKilometers()
+        {
+            return Kilometers;
+        }
+        public override string GetVisitedPlaces()
+        {
+            return VisitedPlaces;
+        }
+
+        public override string GetInfo()
+        {
+            return $"Train ({Manufacturer}, {NumberOfWagons}) : {Kilometers} km";
+        }
+    }
+}
