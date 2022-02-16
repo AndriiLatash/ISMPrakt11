4c366340c94291db76a6b9b5fe7df7642e4fcd7d
Andrii <latasandrej@gmail.com>
2022-02-16 23:49:51 +0200

    Created abstract class Transport

---
+++ ClassLibrary/Transport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    public abstract class Transport
+    {
+
+        public double Kilometers { set; get; }
+        public string VisitedPlaces;
+        public abstract double GetKilometers();
+        public abstract string GetVisitedPlaces();
+        public abstract string GetInfo();
+        public void Drive(string visitedPlaces, double km)
+        {
+            Kilometers += km;
+            VisitedPlaces = visitedPlaces;
+        }
+
+        public void ZeroingMilege()
+        {
+            Kilometers = 0;
+        }
+        public double GetMileage()
+        {
+            return Kilometers;
+        }
+    }
+
+}
+
