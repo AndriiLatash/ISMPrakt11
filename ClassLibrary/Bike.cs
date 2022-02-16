350456484b3d212cdde5b5d4f134d182425d91cc
Andrii <latasandrej@gmail.com>
2022-02-17 00:04:07 +0200

    Created class Bike

---
+++ ClassLibrary/Bike.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Bike : Transport
+    {
+
+        public string Model { set; get; }
+        public string Color { set; get; }
+        public Bike(double kilometers, string model, string color)
+        {
+            Kilometers = kilometers;
+            Model = model;
+            Color = color;
+        }
+        public Bike(string visitedPlaces)
+        {
+            VisitedPlaces = visitedPlaces;
+        }
+        public override double GetKilometers()
+        {
+            return Kilometers;
+        }
+        public override string GetVisitedPlaces()
+        {
+            return VisitedPlaces;
+        }
+
+        public override string GetInfo()
+        {
+            return $"Bike ({Model}, {Color}) : {Kilometers} km";
+        }
+    }
+}
