08e18bcdd9cd2621f73ab32c2622c5ba7f26d1bb
Andrii <latasandrej@gmail.com>
2022-02-16 23:56:32 +0200

    Implemented class Car

--- ClassLibrary/Car.cs
+++ ClassLibrary/Car.cs
@@ -2,9 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ClassLibrary
+namespace ConsoleApp8
 {
-    class Car
+    class Car : Transport
     {
+
+        public string Manufacturer { set; get; }
+        public string Color { set; get; }
+
+        public Car(double kilometers, string manufacturer, string color)
+        {
+            Kilometers = kilometers;
+            Manufacturer = manufacturer;
+            Color = color;
+        }
+        public Car(string visitedPlaces)
+        {
+            VisitedPlaces = visitedPlaces;
+        }
+        public override double GetKilometers()
+        {
+            return Kilometers;
+        }
+        public override string GetVisitedPlaces()
+        {
+            return VisitedPlaces;
+        }
+
+        public override string GetInfo()
+        {
+            return $"Taxi ({Manufacturer}, {Color}) : {Kilometers} km";
+        }
+
     }
 }
